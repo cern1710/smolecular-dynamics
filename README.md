8695d8ae347f9cac18496c69c60154b22aa01b7b
Samuel Wu <cernunnos1710@gmail.com>
2023-06-10 19:17:35 +1000

    incorporated GLM into code; changed README

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ This repo contains folders for basic molecular dynamics simulations. SML2 and C+
 
 ## Description
 
-- **particles**: A test playground on particle simulations for developing and exploring C++ and SML2 for myself.
+- **particles**: A test playground on particle simulations using different C++ libraries.
--- particles/particle.h
+++ particles/particle.h
@@ -1,20 +1,21 @@
-#include <SDL2/SDL.h>
+#include <glm/glm.hpp>
 
 const int WINDOW_WIDTH = 1080;
 const int WINDOW_HEIGHT = 720;
 const double GRAVITY = 0.01;
 
 struct Particle {
-    double x, y;
-    double vx, vy;
+    glm::vec2 position;
+    glm::vec2 velocity;
     int radius = 3;
 
-    Particle(double x, double y, double vx, double vy) : x(x), y(y), vx(vx), vy(vy) {}
+    Particle(double x, double y, double vx, double vy) : position(x, y), velocity(vx, vy) {}
 
     void drawCircle(SDL_Renderer* renderer, int x_centre, int y_centre, int radius) {
         for (int w = 0; w < radius * 2; w++)
             for (int h = 0; h < radius * 2; h++) {
-                int dx = radius - w, dy = radius - h;   // horizontal and vertical offset
+                int dx = radius - w;  // horizontal offset
+                int dy = radius - h;  // vertical offset
 
                 if ((dx*dx + dy*dy) <= (radius * radius))
                     SDL_RenderDrawPoint(renderer, x_centre + dx, y_centre + dy);
@@ -22,18 +23,17 @@ struct Particle {
     }
     
     void update() {
-        x += vx;
-        y += vy;
-        vy += GRAVITY; // gravity effect
+        position += velocity;
+        velocity.y += GRAVITY;
 
-        if (y > WINDOW_HEIGHT - radius) { // consider radius for bouncing back
-            y = WINDOW_HEIGHT - radius;
-            vy *= -0.5;
+        if (position.y > WINDOW_HEIGHT - radius) { // consider radius for bouncing back
+            position.y = WINDOW_HEIGHT - radius;
+            velocity.y *= -0.5;
         }
     }
 
     void render(SDL_Renderer* renderer) {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        drawCircle(renderer, x, y, radius);
+        drawCircle(renderer, position.x, position.y, radius);
     }
 };
